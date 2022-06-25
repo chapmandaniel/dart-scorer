9266d413ebbd64a9f1cbe6e69ea0b104366a66fb
chapmandaniel <daniel.chapman@gmail.com>
2022-06-25 21:10:54 -0230

    commandline version completed - needs visit class and integration - needs UI created

--- classes.js
+++ classes.js
@@ -16,31 +16,25 @@ class Match{
         this.legsToWin = legsToWin;
         this.legsPlayed = 0;
 
-        //two registered players
-        this.playerA = new Player(name1);
+        this.playerA = new Player(name1);   //two registered players
         this.playerB = new Player(name2);
 
-        //player 1 and player 2 list
-        this.players = [];
+        this.players = [];      //player 1 and player 2 array for order of turns after coin toss
+        this.coinTossWinner = null;     //the winner of the coin toss to go first
+        this.atTheOche = null;        //the player who is at the toe line (oche)
 
-        //the winner of the coin toss to go first
-        this.coinTossWinner = null;
-        this.atTheOche = null;  //******** temporary this needs to be sey dynamically ******************
     }
 
-    // set the coin toss winner
-    coinToss(playsFirst){
-        if(playsFirst === this.playerA){
-            this.players[0] = this.playerA;
-            this.players[1] = this.playerB;
-        }else{
-            this.players[0] = this.playerB;
-            this.players[1] = this.playerA;
-        }
+    coinToss(playsFirst){         // set the coin toss winner
+
+        this.players[0] = (playsFirst === this.playerA) ? this.playerA : this.playerB;
+        this.players[1] = (playsFirst === this.playerA) ? this.playerB : this.playerA;
         this.upFirst();
     }
 
-    throwDarts(score){
+
+    throwDarts(score){  // method that performs a players turn
+
         if(!isGameOver()){
             this.atTheOche.score -= score;
             this.alternateTurns();
@@ -49,37 +43,44 @@ class Match{
         }
     }
 
-    upFirst(){
+
+    upFirst(){ // method for setting the player who goes first
         let currentGame = this.legsPlayed + 1;
         this.atTheOche = (currentGame % 2) ? this.players[0] : this.players[1];
     }
 
-    alternateTurns(){
+
+    alternateTurns(){ // method for alternating turns
         this.atTheOche = (this.atTheOche === this.players[0]) ? this.players[1] : this.players[0];
     }
 
-    isLegOver(){
+
+    isLegOver(){  // method for checking if a leg is over, and if so, check if the game is over
         let over = false;
+
         if(this.players[0].score === 0){
             this.players[0].legs++;
             console.log(this.players[0].name + " wins leg " + this.players[0].legs);
             over = true;
         }
+
         if(this.players[1].score === 0) {
             this.players[1].legs++;
             console.log(this.players[1].name + " wins leg " + this.players[1].legs);
             over = true;
         }
+
         if(over){
             this.legsPlayed++;
-            isGameOver() ? null : this.startNewLeg();
+            isGameOver() ? null : this.startNewLeg("Leg over, starting new leg");
         }
     }
 
-    startNewLeg(){
+
+    startNewLeg(message = ""){  // method to start a new leg
         this.players.forEach(function(player){player.score = 501});
         this.upFirst();
-        updateInterface("Leg over, starting new leg");
+        updateInterface(message);
     }
 
     printScore(){
